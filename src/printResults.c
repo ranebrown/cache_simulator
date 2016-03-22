87d342336f34e8cf9e02da69743b7fe212e7d572
ranebrown <rane.brown@gmail.com>
2016-03-22 08:37:36 -0600

    added struct to pass in memory parameters

--- src/printResults.c
+++ src/printResults.c
@@ -6,7 +6,7 @@
  */
 #include "printResults.h"
 
-int printResults(char *trace)
+int printResults(char *trace, memInfo *mem)
 {
     FILE *fp;               // file pointer
     char name[128];         // directory path to write to
@@ -23,21 +23,21 @@ int printResults(char *trace)
     fp = fopen(name, "w+");
 
     // print to file
-    fprintf(fp,"------------------------------------------------------------------------------------------\n");
-    fprintf(fp, "%20s %30s\n", trace, "Simulation Results");
-    fprintf(fp,"------------------------------------------------------------------------------------------\n");
-    fprintf(fp,"\n");
-    fprintf(fp,"%60s\n","Memory System Information");
-    fprintf(fp,"%60s\n","-------------------------");
-    fprintf(fp,"%20s %20s %20s %20s\n","hierarchy","size","ways","block size");
-    fprintf(fp,"%20s %20s %20s %20s\n","---------","----","----","----------");
-    fprintf(fp,"%20s %20d %20d %20d\n","L1 data",8192,2,32);
-    fprintf(fp,"%20s %20d %20d %20d\n","L1 instruction",8192,2,32);
-    fprintf(fp,"%20s %20d %20d %20d\n","L2 cache",8192,2,32);
-    fprintf(fp,"\n");
-    fprintf(fp,"%20s %20s %20s %20s\n","","ready time","chunk size","chunk time");
-    fprintf(fp,"%20s %20s %20s %20s\n","","----------","----------","----------");
-    fprintf(fp,"%20s %20d %20d %20d\n","Main memory",50,8,15);
+    fprintf( fp, "------------------------------------------------------------------------------------------\n");
+    fprintf( fp, "%20s %30s\n", trace, "Simulation Results");
+    fprintf( fp, "------------------------------------------------------------------------------------------\n");
+    fprintf( fp, "\n");
+    fprintf( fp, "%60s\n","Memory System Information");
+    fprintf( fp, "%60s\n","-------------------------");
+    fprintf( fp, "%20s %20s %20s %20s\n", "hierarchy", "size", "ways", "block size");
+    fprintf( fp, "%20s %20s %20s %20s\n", "---------", "----", "----", "----------");
+    fprintf( fp, "%20s %20d %20d %20d\n", "L1 data", mem->L1dSize, mem->L1dWays, mem->L1dBlock);
+    fprintf( fp, "%20s %20d %20d %20d\n", "L1 instruction", mem->L1iSize, mem->L1iWays, mem->L1iBlock);
+    fprintf( fp, "%20s %20d %20d %20d\n", "L2 cache", mem->L2Size, mem->L2Ways, mem->L2Block);
+    fprintf( fp, "\n");
+    fprintf( fp, "%20s %20s %20s %20s\n", "hierarchy", "ready time", "chunk size", "chunk time");
+    fprintf( fp, "%20s %20s %20s %20s\n", "---------", "----------", "----------", "----------");
+    fprintf( fp, "%20s %20d %20d %20d\n", "Main memory", mem->readyT, mem->chunkS, mem->chunkT);
 
     // close file
     fclose(fp);
