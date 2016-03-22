8b12c3f75edf3446432533c20ee9fd808adbfe6b
brdo5692 <brdo5692@colorado.edu>
2016-03-21 23:29:46 -0600

    Changed how the config files are setup, began parsing function

---
+++ config/config.h
@@ -0,0 +1,40 @@
+/**
+ * @file        config.h
+ * @authors     Rane Brown & Brian Douglass
+ * @date        03-20-2016
+ * @brief       Header file with function prototypes for configuring the cache and memory.
+ */
+
+ #ifndef CONFIG_H
+     #define CONFIG_H
+
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * @struct memInfo
+ * @brief contains variables about the memory system configuration in use
+ */
+typedef struct
+{
+    int L1dType;    /// L1 data cache type
+    int L1dSize;    /// L1 data cache size
+    int L1dWays;    /// L1 data cache ways
+    int L1dBlock;   /// L1 data block size
+    int L1iType;    /// L1 instruction type
+    int L1iSize;    /// L1 instruction cache size
+    int L1iWays;    /// L1 instruction cache ways
+    int L1iBlock;   /// L1 instruction block size
+    int L2Type;     /// L2 cache type
+    int L2Size;     /// L2 cache size
+    int L2Ways;     /// L2 cache ways
+    int L2Block;    /// L2 cache block size
+    int addrsendT;   /// main memory address send time
+    int readyT;     /// main memory ready time
+    int chunkT;     /// main memory chunk time
+    int chunkS;     /// main memory chunk size
+} memInfo;
+
+void setValues(char inStr[10]);
+
+#endif //CONFIG_H
--- config/config_test.c
+++ config/config_test.c
@@ -1,10 +1,12 @@
-#include <stdio.h>
-#include <string.h>
+
+#include "config.h"
+
+memInfo cache;
 
 int main (int argc, char ** argv)
 {
     FILE *config;
-    char inStr[20];
+    char inStr[11];
 
     (argc == 2) ? (config = fopen(argv[1],"r"))
                 : (config = fopen("default","r"));
@@ -12,7 +14,38 @@ int main (int argc, char ** argv)
     fscanf(config, "%s", inStr);
     printf("-- %s --\n",inStr);
 
-    printf("string length: %lu\n",strlen(inStr));
+    setValues(inStr);
+
+    //printf("string length: %lu\n",strlen(inStr));
 
     return 0;
+}
+
+void setValues(char inStr[])
+{
+    /* L1 data */
+    cache.L1dType   = inStr[0]-'0';
+    cache.L1dWays   = inStr[1]-'0';
+    cache.L1dSize   = inStr[2]-'0';
+    cache.L1dBlock  = 32;          // TODO
+
+    /* L1 instruction */
+    cache.L1iType   = inStr[3]-'0';
+    cache.L1iWays   = inStr[4]-'0';
+    printf("L1iWays: %d\n",cache.L1dType);
+    cache.L1iSize   = inStr[5]-'0';
+    cache.L1iBlock  = 32;          // TODO
+
+    /* L2 */
+    cache.L2Type    = inStr[6]-'0';
+    cache.L2Ways    = inStr[7]-'0';
+    cache.L2Size    = (inStr[8]-'0')*10 + (inStr[9]-'0');
+    printf("L2 size: %d\n",cache.L2Size);
+    cache.L2Block   = 64;      // TODO
+
+    /* Main Memory */
+    cache.addrsendT = 10;
+    cache.readyT    = 50;
+    cache.chunkT    = 15;
+    cache.chunkS    = 8;
 }
