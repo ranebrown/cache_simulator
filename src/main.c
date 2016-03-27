b132603b02b9696d185b1aff0966b9a8588855a4
ranebrown <rane.brown@gmail.com>
2016-03-27 18:16:59 -0400

    added tests for specifying a trace and config file

--- src/main.c
+++ src/main.c
@@ -6,11 +6,30 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "printResults.h"
+#include "readTrace.h"
 
 int main(int argc, char *argv[])
 {
-    (void)argc;
-    (void)argv;
+    FILE *config;                       /// file pointer to configuration file
+    char inStr[20];                     /// the line read in from configuration file
+    char op;                            /// type of operation - read or write or instruction
+    unsigned long long int addr;        /// memory address
+    unsigned int bs;                    /// byte size - number of bytes referenced by request
+    int res = 0;                        /// result of trace read
+
+    (argc == 2) ? (config = fopen(argv[1],"r"))
+                : (config = fopen("default","r"));
+    fscanf(config, "%s", inStr);
+    printf("-- %s --\n",inStr);
+
+    while(res == 0)
+    {
+        res = readTrace(&op, &addr, &bs);
+        printf("%c %llx %d\n" ,op ,addr ,bs);
+
+    }
 
     return EXIT_SUCCESS;
 }
