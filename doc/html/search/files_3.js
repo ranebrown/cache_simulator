4df53bff93fef4ebcf6570fece85dec8d78751e1
ranebrown <rane.brown@gmail.com>
2016-03-30 22:35:23 -0600

    doxygen html files

---
+++ doc/html/search/files_3.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['readtrace_2ec',['readTrace.c',['../read_trace_8c.html',1,'']]],
+  ['readtrace_2eh',['readTrace.h',['../read_trace_8h.html',1,'']]]
+];
---
+++ doc/html/search/functions_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['readtrace',['readTrace',['../read_trace_8c.html#a8b90063d0fcee263d7a789b957c7f1cb',1,'readTrace(char *op, unsigned long long int *addr, unsigned int *bs):&#160;readTrace.c'],['../read_trace_8h.html#a8b90063d0fcee263d7a789b957c7f1cb',1,'readTrace(char *op, unsigned long long int *addr, unsigned int *bs):&#160;readTrace.c']]],
+  ['readtrace_5fsuite',['readTrace_suite',['../test__read_trace_8c.html#af72b1b0d89e85bcfcac4a825158f4a4b',1,'test_readTrace.c']]]
+];
