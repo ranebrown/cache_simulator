aca1243128b02bff2f478c497fbc7adb037b3409
ranebrown <rane.brown@gmail.com>
2016-04-04 08:02:05 -0600

    removed memInfo struct (now in config)

--- inc/printResults.h
+++ inc/printResults.h
@@ -6,37 +6,12 @@
  */
 #ifndef PRINT_RESULTS_H
     #define PRINT_RESULTS_H
-    //#define USEPRINTSTRUCT 0
 
     #include <stdio.h>
     #include <stdlib.h>
     #include <string.h>
     #include <errno.h>
     #include "config.h"
-
-    #ifdef USEPRINTSTRUCT // Keep this here until it is debugged enough
-    /**
-     * @struct memInfo
-     * @brief contains variables about the memory system configuration in use
-     */
-    typedef struct
-    {
-        //char cacheName[32]; ///Holds the config filename, helpful for printing
-        int L1dSize;    /// L1 data cache size
-        int L1dWays;    /// L1 data cache ways
-        int L1dBlock;   /// L1 data block size
-        int L1iSize;    /// L1 instruction cache size
-        int L1iWays;    /// L1 instruction cache ways
-        int L1iBlock;   /// L1 instruction block size
-        int L2Size;     /// L2 cache size
-        int L2Ways;     /// L2 cache ways
-        int L2Block;    /// L2 cache block size
-        //int addrsendT;  /// main memory address send time
-        int readyT;     /// main memory ready time
-        int chunkT;     /// main memory chunk time
-        int chunkS;     /// main memory chunk size
-    } memInfo;
-    #endif
 
     /**
      * @brief prints the results of the simulation to a file
