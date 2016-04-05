0171415587e9d214b32db9de7653c9499e68db28
ranebrown <rane.brown@gmail.com>
2016-04-04 20:47:37 -0600

    testing these still

--- inc/cache.h
+++ inc/cache.h
@@ -1,8 +1,8 @@
 /**
- * @file        directMapped.h
+ * @file        cache.h
  * @authors     Rane Brown & Brian Douglass
  * @date        04-01-2016
- * @brief       Header file with function prototypes and structs related to a direct mapped cache
+ * @brief       Header file with function prototypes and structs used for cache operations
  */
 #ifndef DIRECT_MAPPED_H
     #define DIRECT_MAPPED_H
@@ -11,6 +11,12 @@
     #include <stdlib.h>
     #include <math.h>
     #include "config.h"
+
+    /**
+     * @typedef ulli
+     * @brief typedef for an unsigned long long int because that is too much to write
+     */
+    typedef unsigned long long int ulli;
 
     /**
      * @struct block
@@ -29,21 +35,53 @@
      */
     typedef struct
     {
-        ulli    totExecT;      ///< total execution time for the simulation
-        ulli    totRefs;       ///< total number of references = data + instruction
-        ulli    instRefs;      ///< number of instruction references
-        ulli    dataRefs;      ///< number of data references = read + writes
-        ulli    readCount;     ///< number of data reads
-        ulli    writeCount;    ///< number of data writes
-        float   percInst;      ///< percentage of references that are instructions
-        float   percDRead;     ///< percentage of references that are data reads
-        float   percDWrite;    ///< percentage of references that are data writes
+        ulli    totExecT;       ///< total execution time for the simulation
+        ulli    totRefs;        ///< total number of references = data + instruction
+        ulli    instRefs;       ///< number of instruction references
+        ulli    dataRefs;       ///< number of data references = read + writes
+        ulli    dataReads;      ///< number of data reads
+        ulli    dataWrites;     ///< number of data writes
+        float   percRefInst;    ///< percentage of references that are instructions
+        float   percRefDRead;   ///< percentage of references that are data reads
+        float   percRefDWrite;  ///< percentage of references that are data writes
+        ulli    cycleDRead;     ///< number of cycles spent on data reads
+        ulli    cycleDWrite;    ///< number of cycles spent on data writes
+        ulli    cycleInst;      ///< number of cycles spent on instructions
+        float   percCycleDR;    ///< percentage of cycles that are data reads
+        float   percCycleDW;    ///< percentage of cycles that are data writes
+        float   percCycleInst;  ///< percentage of cycles that are instructions
+        ulli    idealExecT;     ///< ideal execution time
+        ulli    idealMisExecT;  ///< ideal mis-aligned exectition time
+        float   cpi;            ///< actual CPI
+        float   idealCpi;       ///< ideal CPI
+        float   idealMisCpi;    ///< ideal mis-aligned CPI
+        ulli    hitL1i;         ///< hit count L1 instruction cache
+        ulli    hitL1d;         ///< hit count L1 data cache
+        ulli    hitL2;          ///< hit count L2 cache
+        ulli    missL1i;        ///< miss count L1 instruction cache
+        ulli    missL1d;        ///< miss count L1 data cache
+        ulli    missL2;         ///< miss count L2 cache
+        ulli    totalReqL1i;    ///< total requests L1 instruction cache
+        ulli    totalReqL1d;    ///< total requests L1 data cache
+        ulli    totalReqL2;     ///< total requests L2 cache
+        float   hitRateL1i;     ///< hit rate percentage L1 instruction cache
+        float   hitRateL1d;     ///< hit rate percentage L1 data cache
+        float   hitRateL2;      ///< hit rate percentage L2 cache
+        float   missRateL1i;    ///< miss rate percentage L1 instruction cache
+        float   missRateL1d;    ///< miss rate percentage L1 data cache
+        float   missRateL2;     ///< miss rate percentage L2 cache
+        ulli    kickoutL1i;     ///< total kickouts L1 instruction cache
+        ulli    kickoutL1d;     ///< total kickouts L1 data cache
+        ulli    kickoutL2;      ///< total kickouts L2 cache
+        ulli    dirtyKickL1i;   ///< total kickouts L1 instruction cache
+        ulli    dirtyKickL1d;   ///< total kickouts L1 data cache
+        ulli    dirtyKickL2;    ///< total kickouts L2 cache
+        ulli    transfersL1i;   ///< number of transfers L1 instruction cache
+        ulli    transfersL1d;   ///< number of transfers L1 data cache
+        ulli    transfersL2;    ///< number of transfers L2 cache
+        ulli    VChitL1i;       ///< victim cache hit count L1 instruction cache
+        ulli    VChitL1d;       ///< victim cache hit count L1 data cache
+        ulli    VChitL2;        ///< victim cache hit ocunt L2 cache
     } performance;
-
-    /**
-     * @typedef ulli
-     * @brief typedef for an unsigned long long int because its a pain to write that so often
-     */
-    typedef unsigned long long int ulli;
 
 #endif // DIRECT_MAPPED_H
--- src/cache.c
+++ src/cache.c
@@ -1,35 +1,35 @@
 /**
- * @file        directMapped.c
+ * @file        cache.c
  * @authors     Rane Brown & Brian Douglass
  * @date        04-01-2016
- * @brief       Functions related to a direct mapped cache
+ * @brief       Functions related to all cache operations
  */
 
-#include "directMapped.h"
+#include "cache.h"
 
-int L1(char op, unsigned long long int addr, unsigned int numBytes, block *L1, memInfo *mem)
+int L1(char op, unsigned long long int addr, unsigned int numBytes, block **l1cache, memInfo *mem)
 {
     // index and tag variables
     int currIndx = 0;
     unsigned long long currTag = 0;
 
     // calculate the word aligned start address
-    unsigned long long startAddr = addr & 0xFFFFFFFFFFFFFFFC;
+    unsigned long long currAddr = addr & 0xFFFFFFFFFFFFFFFC;
 
     // calculate the word aligned end address
     unsigned long long endAddr = (addr + numBytes) & 0xFFFFFFFFFFFFFFFC;
 
     // TODO check for type of op -> create different functions for each
 
-    while(startAddr <= endAddr)
+    while(currAddr <= endAddr)
     {
         // calculate the index and tag for the current address
         // TODO can use log2() for this calculation?
         currIndx = (currAddr / mem->L1dBlock) % (mem->L1dSize / mem->L1dBlock);
-        currTag = currAddr >> (64 - (log2(mem->l1dSize / mem->L1dBlock) + log2(mem->L1dBlock)));
+        currTag = currAddr >> (64 - ((int)log2(mem->L1dSize / mem->L1dBlock) + (int)log2(mem->L1dBlock)));
 
         // check if the current tag is in the cache
-        if( (L1[currIndx]->tag == currTag) && (L1[currIndx]->valid == 1) )
+        if( (l1cache[currIndx]->tag == currTag) && (l1cache[currIndx]->valid == 1) )
         {
             // hit
             // TODO stats
@@ -41,6 +41,7 @@ int L1(char op, unsigned long long int addr, unsigned int numBytes, block *L1, m
         }
 
         // increment address 4 bytes
-        startAddr += 4;
+        currAddr += 4;
     }
+    return EXIT_SUCCESS;
 }
