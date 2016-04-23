b3b552556d22f3bbcf70b449b4874d926c2753d4
ranebrown <rane.brown@gmail.com>
2016-04-23 17:07:22 -0600

    individual run files for trace lenghts

---
+++ build/runall_1M.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+make clean
+
+tpath="../traces/traces_1M/"
+cpath="../config/"
+
+for trace in sjeng astar bzip2 gobmk libquantum omnetpp
+do
+    for config in default L1-2Way All-2way All-4way L1-8way L1-small L1-small-4way All-small All-FA
+    do
+        make run trace=$tpath$trace.gz conf=$cpath$config.txt
+    done
+done
+
+echo complete
---
+++ build/runall_short.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+make clean
+
+tpath="../traces/traces_short/"
+cpath="../config/"
+
+for trace in tr1 tr2 tr3 tr4 tr5 tr6
+do
+    for config in default L1-2Way All-2way All-4way L1-8way L1-small L1-small-4way All-small All-FA
+    do
+        make run trace=$tpath$trace.gz conf=$cpath$config.txt
+    done
+done
+
+echo complete
