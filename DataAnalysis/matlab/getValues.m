7d99f5e85e9580c89b18529099289b7e5918db16
ranebrown <rane.brown@gmail.com>
2016-04-25 22:00:43 -0600

    matlab files to read in simulation data

---
+++ DataAnalysis/matlab/getValues.m
@@ -0,0 +1,48 @@
+function [ res ] = getValues( trace )
+% takes a specific memory configuration i.e. astar.default and returns a
+% struct containing specific values from the simulation for that
+% configuration
+
+res.L1size              =       trace(1);
+res.L1ways              =       trace(2);
+res.L1block             =       trace(3);
+res.L2size              =       trace(4);
+res.L2ways              =       trace(5);
+res.L2block             =       trace(6);
+res.execTime            =       trace(7);
+res.totalRefs           =       trace(8);
+res.numInstRefs         =       trace(9);
+res.numDataRefs         =       trace(10);
+res.numReads            =       trace(11);
+res.numWrites           =       trace(12);
+res.numInst             =       trace(13);
+res.cpi                 =       trace(14);
+res.idealExecT          =       trace(15);
+res.idealCPI            =       trace(16);
+res.idealMisAExecT      =       trace(17);
+res.idealMisACPI        =       trace(18);
+res.L1ihits             =       trace(19);
+res.L1dhits             =       trace(20);
+res.L2hits              =       trace(21);
+res.L1imiss             =       trace(22);
+res.L1dmiss             =       trace(23);
+res.L2miss              =       trace(24);
+res.L1iTotReqs          =       trace(25);
+res.L1dTotReqs          =       trace(26);
+res.L2TotReqs           =       trace(27);
+res.L1iKicks            =       trace(28);
+res.L1dKicks            =       trace(29);
+res.L2Kicks             =       trace(30);
+res.L1iDrtyKcks         =       trace(31);
+res.L1dDrtyKcks         =       trace(32);
+res.L2DrtyKcks          =       trace(33);
+res.L1iVChit            =       trace(34);
+res.L1dVChit            =       trace(35);
+res.L2VChit             =       trace(36);
+res.L1iCost             =       trace(37);
+res.L1dCost             =       trace(38);
+res.L2Cost              =       trace(39);
+res.totCost             =       trace(40);
+
+end
+
---
+++ DataAnalysis/matlab/readData.m
@@ -0,0 +1,78 @@
+function [ astar, bzip2, gobmk, libquantum, omnetpp, sjeng ] = readData(  )
+% reads in simulation results from a csv file
+
+% read simulation data from csv file
+M = csvread('../csv/allData_noName.csv');
+
+% separate data into individual components
+
+% astar
+astar.All_2way              =       M(1,:);
+astar.All_4way              =       M(2,:);
+astar.All_FA                =       M(3,:);
+astar.All_small             =       M(4,:);
+astar.default               =       M(5,:);
+astar.L1_2way               =       M(6,:);
+astar.L1_8way               =       M(7,:);
+astar.L1_small_4way         =       M(8,:);
+astar.L1_small              =       M(9,:);
+
+% bzip2
+bzip2.All_2way              =       M(10,:);
+bzip2.All_4way              =       M(11,:);
+bzip2.All_FA                =       M(12,:);
+bzip2.All_small             =       M(13,:);
+bzip2.default               =       M(14,:);
+bzip2.L1_2way               =       M(15,:);
+bzip2.L1_8way               =       M(16,:);
+bzip2.L1_small_4way         =       M(17,:);
+bzip2.L1_small              =       M(18,:);
+
+% gobmk
+gobmk.All_2way              =       M(19,:);
+gobmk.All_4way              =       M(20,:);
+gobmk.All_FA                =       M(21,:);
+gobmk.All_small             =       M(22,:);
+gobmk.default               =       M(23,:);
+gobmk.L1_2way               =       M(24,:);
+gobmk.L1_8way               =       M(25,:);
+gobmk.L1_small_4way         =       M(26,:);
+gobmk.L1_small              =       M(27,:);
+
+% libquantum
+libquantum.All_2way         =       M(28,:);
+libquantum.All_4way         =       M(29,:);
+libquantum.All_FA           =       M(30,:);
+libquantum.All_small        =       M(31,:);
+libquantum.default          =       M(32,:);
+libquantum.L1_2way          =       M(33,:);
+libquantum.L1_8way          =       M(34,:);
+libquantum.L1_small_4way    =       M(35,:);
+aslibquantumtar.L1_small    =       M(36,:);
+
+% omnetpp
+omnetpp.All_2way            =       M(37,:);
+omnetpp.All_4way            =       M(38,:);
+omnetpp.All_FA              =       M(39,:);
+omnetpp.All_small           =       M(40,:);
+omnetpp.default             =       M(41,:);
+omnetpp.L1_2way             =       M(42,:);
+omnetpp.L1_8way             =       M(43,:);
+omnetpp.L1_small_4way       =       M(44,:);
+omnetpp.L1_small            =       M(45,:);
+
+% sjeng
+sjeng.All_2way              =       M(46,:);
+sjeng.All_4way              =       M(47,:);
+sjeng.All_FA                =       M(48,:);
+sjeng.All_small             =       M(49,:);
+sjeng.default               =       M(50,:);
+sjeng.L1_2way               =       M(51,:);
+sjeng.L1_8way               =       M(52,:);
+sjeng.L1_small_4way         =       M(53,:);
+sjeng.L1_small              =       M(54,:);
+
+% sjeng extra
+sjeng.bw16                  =       M(55,:);
+sjeng.bw32                  =       M(56,:);
+sjeng.bw64                  =       M(57,:);
